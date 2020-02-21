42b316bb0eb9b0845d18b86f89bfac793c37d0a7
Suborna Jahan <suboorna@gmail.com>
2020-02-22 00:40:23 +0100

    just started the dolas app, player around with the ui

---
+++ src/components/Balance.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Balance = () => {
+    return (
+        <>
+            <h4>
+                Your Balance
+            </h4>
+            <h1>
+                £0.00
+            </h1>
+        </>
+    )
+}
