01d3029861441a5aeac2816d8369370a830b8d57
Suborna Jahan <suboorna@gmail.com>
2020-02-24 22:27:04 +0100

    the history section shows text and amount with their designated colour

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import { Balance } from './components/Balance';
 import { IncomeExpenses } from './components/IncomeExpenses';
 import { TransactionList } from './components/TransactionList';
 import { AddTransaction } from './components/AddTransaction';
+import { GlobalProvider } from './context/GlobalState';
 
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <Header />
-            <div className="container">
-                <Balance />
-                <IncomeExpenses />
-                <TransactionList />
-                <AddTransaction />
-            </div>
-        </div>
+            <GlobalProvider>
+                <Header />
+                <div className="container">
+                    <Balance />
+                    <IncomeExpenses />
+                    <TransactionList />
+                    <AddTransaction />
+                </div>
+            </GlobalProvider>
     );
 }
 
